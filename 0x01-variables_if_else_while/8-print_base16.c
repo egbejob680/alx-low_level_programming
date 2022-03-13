d1ddca5aa519e2602f9f0971a8d0037147b048d4
egbejob680 <jobegbe39@gmail.com>
2022-03-13 01:43:28 +0100

    corrected

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
    char i;
    for (n = 48; n < 58; n++)
    {
-       putchar(n);
+    putchar(n);
    }
    for (i = 'a'; i <= 'f'; i++)
    {
-       putchar(i);
+    putchar(i);
    }
    putchar('\n');
    return(0);
