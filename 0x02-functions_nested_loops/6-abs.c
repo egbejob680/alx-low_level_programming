2074e4e936d47caaea5d0c2200b7dd886e25b154
egbejob680 <jobegbe39@gmail.com>
2022-03-17 00:32:30 +0100

    changed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _islower -check for lower case character, followed by a new line
+* @n: the int to  checks
+* Return the absolute value of int
+*/
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+return (-n);
+}
