42d6fb48a84057fb9b41ee333b07f77043a9968f
egbejob680 <jobegbe39@gmail.com>
2022-03-16 23:08:14 +0100

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main- prints alphabets, followed by a new line
+* Return - Always 0(success)
+*/
+void print_alphabet(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar(ch);
+}
