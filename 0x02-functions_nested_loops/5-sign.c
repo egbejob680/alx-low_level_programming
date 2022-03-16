53f37592e912daaf9860117888a41a9a89d0749a
egbejob680 <jobegbe39@gmail.com>
2022-03-17 00:24:00 +0100

    changed mode

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _islower -check for lower case character, followed by a new line
+* @n: the int to  checks
+* Return 1 and print + , 0 and print 0, -1 and print -
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if(n == 0)
+{
+_putchar(48);
+return (0);   
+}
+else if(n < 0)
+{
+_putchar('-')
+}
+return (-1);
+}
