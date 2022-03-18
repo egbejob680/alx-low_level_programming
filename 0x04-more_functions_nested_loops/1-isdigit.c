96a95042f3defc1b68d8832ca686b9c5cd1ab8fe
egbejob680 <jobegbe39@gmail.com>
2022-03-18 22:39:42 +0100

    check for digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/* 
+* isdigit- check for a digit
+*@c: the int  to be checked
+* return 1 if c is uppercase ,0 otherwie
+*/
+int _isupper(int c)
+{
+return (c >= '0' && c <='9');
+}
