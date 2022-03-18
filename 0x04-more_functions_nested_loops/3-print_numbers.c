18362013a89c140712077c79d43f623726e99eb7
egbejob680 <jobegbe39@gmail.com>
2022-03-18 22:49:17 +0100

    print 1 to 10

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* 
+* print_numbers- print num 0 to 9
+* followed by a new line
+*/
+void print_numbers(void)
+{
+int ;
+for (i = 0; i<10; i++)
+{
+_putchar(1 +'0');
+}
+_putchar('\n');
+}
