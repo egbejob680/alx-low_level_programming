029dc8ad9af83b9940ca40b35e8013cfcfbd09df
egbejob680 <jobegbe39@gmail.com>
2022-03-24 22:03:15 +0100

    encode string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* leet- encode a string
+* @s: string to be encoded 
+* 
+*/
+char *leet(char *s)
+{
+int i, j;
+char *a = "aAeEoOtTiL";
+char *b = "4433007711";
+for (i=0; s[i]!='\0'; i++)
+{
+for (j=0; j<10; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+}
+}
+}
+return (s);
+}
