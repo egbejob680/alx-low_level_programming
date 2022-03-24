b4e340e97b783418c8f7f0b0d80cb018d5225f6e
egbejob680 <jobegbe39@gmail.com>
2022-03-24 21:35:30 +0100

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* string_toupper- change lower case letter to upper case
+* @s string to modify
+* 
+*/
+char *string_toupper(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >='a' && s[i] <= 'z')
+s[i] = s[i]-32; 
+}
+return (s);
+}
