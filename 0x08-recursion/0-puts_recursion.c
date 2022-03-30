a9efff64f70e70e6cfdec004cf47ccfa2b22a5cd
egbejob680 <jobegbe39@gmail.com>
2022-03-30 16:56:57 +0100

    print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts_recursion - print a string to stdout
+* @s: string
+* return void
+*/
+
+void _puts_recursion(char *s)
+{
+    if (!(*s))
+    {
+        _putchar('\n');
+        return;
+    }
+    _putchar(*(s++));
+    _puts_recursion(s);
+}
