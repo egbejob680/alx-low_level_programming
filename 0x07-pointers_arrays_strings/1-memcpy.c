e57f442e591bbbc25737ec4d466b6d0e359c92f5
egbejob680 <jobegbe39@gmail.com>
2022-03-30 18:14:38 +0100

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* memcpy
+* @dest: 
+* @src:
+* @n:
+* return point to memory 
+*/
+char *_memcpy(char *desk, char *src, unsigned int n)
+{
+unsigned int i = 0;
+while (src[i] != '\0')
+{
+if (i < n)
+dest[i] = s[i];
+else
+break;
+}
+while (dest[i] != '\0')
+{
+if (i<)
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
