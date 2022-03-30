9221ae17c8fa5582a24b419541c8c93d228c837e
egbejob680 <jobegbe39@gmail.com>
2022-03-30 20:49:04 +0100

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_chessboard - print a chess board
+* @a: two dimensional array
+* Return: void
+*/
+void print_chessboard(char (*a)[8])
+{
+    int i;
+    int j;
+    int 1 = 0;
+    while (
+        for (i = 0; i
+}
