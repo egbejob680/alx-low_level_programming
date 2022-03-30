fe4167fac78d72433067cf9d161ab78ba65294e8
egbejob680 <jobegbe39@gmail.com>
2022-03-30 22:04:47 +0100

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - update value of an int variable a pointer points to
+* @n: interger pointer 
+* Return: void
+*/
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
