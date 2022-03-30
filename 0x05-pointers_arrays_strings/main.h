a5fcf1dff4dd711538e1f90a60fdc0da9fc2d854
egbejob680 <jobegbe39@gmail.com>
2022-03-30 21:59:10 +0100

    main

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+void _putchar(int);
+void reset_to_98(int *n);
+void swap_int(int *a,int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void swap(char *p, char *q);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int get_number(int *idx, char *s);
+int is_number(char);
+int contain_digit(char *str);
+int number_negative(char *str);
+#endif
