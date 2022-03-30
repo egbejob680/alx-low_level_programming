1c6e0b6cdbe7b725640b8925313a2fcfebc584d2
egbejob680 <jobegbe39@gmail.com>
2022-03-30 17:56:34 +0100

    main

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+int _putchar(int);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *desk, char *src, unsigned int n);
+int *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+int *_strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needel);
+int compare(char *a, char *b);
+void set_string(char **, char *);
+#endif
