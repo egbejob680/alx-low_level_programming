e964dce164aae4f6a763a18abc2ca821ed087075
egbejob680 <jobegbe39@gmail.com>
2022-03-30 22:18:53 +0100

    return length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen - return the length of a string 
+* @s: string
+* Return: length of a string
+*/
+int _strlen(char *s)
+{
+    int index = 0;
+    int count = 0;
+    while ( s[index] != '\0' )
+    {
+        count++;
+        index++;
+    }
+    return (count);
+}
