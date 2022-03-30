dbed835f425af039ff6b6c8783d7aaf479923cb0
egbejob680 <jobegbe39@gmail.com>
2022-03-30 22:36:54 +0100

    reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_rev -print a string in a reverse order
+* @s: string
+* Return: void
+*/
+void print_rev(char *s)
+{
+    char *tp = s;
+    char *i;
+    if (s[0] == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+    while (*tp != '\0')
+    {
+        tp = tp + 1;
+    }
+    for (i = tp-1; i>=s; i -= 1)
+    {
+        _putchar(*i);
+    }
+    _putchar('\n')
+}
