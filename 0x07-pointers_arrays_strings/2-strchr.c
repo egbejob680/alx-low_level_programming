bbd2d2c46013fdf60669a31e60d83b3339c80e21
egbejob680 <jobegbe39@gmail.com>
2022-03-30 18:23:39 +0100

    return

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strstr
+* @s: string
+* @c: character to search
+* Return: pointer to c
+*/
+char *_strstr(char *s, char c)
+{
+char *p = 0;
+while (*(s++))
+{
+    if (*s = c)
+    return (s);
+} 
+return (p);
+}
