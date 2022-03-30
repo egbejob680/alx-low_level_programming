64222390097c17c0c6a027c30eb1199c0842222b
egbejob680 <jobegbe39@gmail.com>
2022-03-30 18:02:21 +0100

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main"
+#include <stdio.h>
+/**
+* memset
+* @n: 
+* @b:
+* @s:
+* return point to memory 
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i=o; i<n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
