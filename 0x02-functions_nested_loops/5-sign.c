3a0a7dd238ba611305d1bf0044f3c71c09ad0b33
egbejob680 <jobegbe39@gmail.com>
2022-10-19 00:20:09 +0100

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
 * _islower -check for lower case character, followed by a new line
 * @n: the int to  checks
-* Return 1 and print + , 0 and print 0, -1 and print -
+* Return: 1 and print + , 0 and print 0, -1 and print -
 */
 int print_sign(int n)
 {
@@ -18,7 +18,7 @@ return (0);
 }
 else if(n < 0)
 {
-_putchar('-')
+_putchar('-');
 }
 return (-1);
 }
