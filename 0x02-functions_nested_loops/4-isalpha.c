270c2dc876464ac023a58551a4d72f600fda459f
egbejob680 <jobegbe39@gmail.com>
2022-10-19 00:25:47 +0100

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,9 @@
 /**
 * _islower -check for lower case character, followed by a new line
 * @c: the character it checks
-* Return 1 if c is a letter,lowercase or uppercase, 0 otherwise
+* Return: 1 if c is a letter,lowercase or uppercase, 0 otherwise
 */
 int  _isalpha(int c)
 {
-return ((c >= 'a' && c<='z') || (c >= 'A' && c<='Z'));
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
