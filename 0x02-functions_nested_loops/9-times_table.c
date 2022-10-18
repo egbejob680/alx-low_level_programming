d4cb3afc6fec8f0335cdb0ce3304b8628619a901
egbejob680 <jobegbe39@gmail.com>
2022-10-19 00:02:19 +0100

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,14 +23,17 @@ _putchar(' ');
 _putchar(k + '0');
 
 } 
+
 else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-}   
+} 
+
 }
 _putchar('\n'); 
 }
+
 }
