3063133dc87768243f3e40fb104038e2147a0cee
egbejob680 <jobegbe39@gmail.com>
2022-10-20 20:24:04 +0100

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,9 +2,9 @@
 /* 
 * isdigit- check for a digit
 *@c: the int  to be checked
-* return 1 if c is uppercase ,0 otherwie
+*return: 1 if c is a digit ,0 otherwie
 */
 int _isdigit( int c)
 {
-return (c >= '0' && c <='9');
+return (c >= '0' && c <= '9');
 }
