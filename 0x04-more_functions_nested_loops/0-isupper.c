22717b9f70842a345f08b1f4a0d8f4ccade1bca7
egbejob680 <jobegbe39@gmail.com>
2022-10-20 20:19:57 +0100

    3

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,9 @@
 /* 
 * isupper- check upper case character
 *@c: the character to be checked
-* return 1 if c is uppercase ,0 otherwie
+*return: 1 if c is uppercase ,0 otherwie
 */
 int _isupper(int c)
 {
-return (c >= 'A' && c <='Z');
+return (c >= 'A' && c <= 'Z');
 }
