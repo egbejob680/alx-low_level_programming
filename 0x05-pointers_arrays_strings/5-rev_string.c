2a5199aec646c97a6635d7e9c58c8760bd01c5f2
egbejob680 <jobegbe39@gmail.com>
2022-10-24 21:49:29 +0100

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,44 +1,53 @@
 #include "main.h"
+
 /**
-* rev_string - reverse a string
-* @s: the string to be reversed
-* Return: viod
-*/
+ * rev_string - reverses a string
+ *
+ * @s: the string to be reversed
+ *
+ * Return: void
+ */
 void rev_string(char *s)
 {
-    int len = _strlen(s);
-    int i = 0;
-    for (i=0; i < (len/2); i++)
-    {
-        swap(s + i, s + (len -1 -i));
-    }
+	int len = _strlen(s);
+	int i = 0;
+
+	for (; i < (len / 2); i++)
+	{
+		swap(s + i, s + (len - 1 - i));
+	}
 }
+
 /**
-* swap - swap two variable values
-* @p: first character
-* @q: second character
-* Return: void
-*/
-void swap(char *p , char *q)
+ * swap - swaps two variable values
+ * @p: first character
+ * @q: second character
+ * Return: void
+ */
+void swap(char *p, char *q)
 {
-    char tmp;
-    tmp = *p;
-    *p = *q;
-    *q = tmp;
+	char tmp;
+
+	tmp = *p;
+	*p = *q;
+	*q = tmp;
 }
+
 /**
-* _strlen -returns size of a string
-* @s: string
-* Return: size of string
-*/
+ * _strlen - returns size of string
+ * @s: string
+ * Return: size of string
+ */
 int _strlen(char *s)
 {
-    int count = 0;
-    int i = 0;
-    while (s[i] != 0)
-    {
-        count++;
-        i++;
-    }
-    return (count);
-}  
+	int count = 0;
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		count++;
+		i++;
+	}
+
+	return (count);
+}
