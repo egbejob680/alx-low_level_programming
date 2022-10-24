dca6c919853b8160d9cbb10299d7cccf11b41646
egbejob680 <jobegbe39@gmail.com>
2022-10-24 21:45:00 +0100

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,42 @@
 #include "main.h"
+
 /**
-* puts_half - print second half of a string
-* @str: string 
-* Return: void
-*/
+ * puts_half - prints second half of a string
+ * @str: string
+ * Return: void
+ */
 void puts_half(char *str)
 {
-    int n = _strlen(str);
-    int start = 1/2;
-    int idx;
-    if (1%2 == 1)
-    start++;
-    for (idx = start; idx < 1; idx++)
-    {
-        _putchar(str[idx]);
+	int l = _strlen(str);
+	int start = l / 2;
+	int idx;
 
-    }
-    _putchar('\0');
+	if (l % 2 == 1)
+		start++;
+
+	for (idx = start; idx < l; idx++)
+	{
+		_putchar(str[idx]);
+	}
+
+	_putchar('\n');
 }
+
 /**
-* _strlen - return length of string
-* @s: string
-* Return: length of string, return 0 if empty
-*/
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of string, returns 0 if empty
+ */
 int _strlen(char *s)
 {
-    int i = 0;
-    int count = 0;
-    while (s[i] != '\0')
-    {
-        count++;
-        i++;
-    }
-    return (count);
+	int i = 0;
+	int count = 0;
+
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+
+	return (count);
 }
