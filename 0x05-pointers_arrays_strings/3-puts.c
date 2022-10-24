a04b5a774cef9963fd214549c8da460a4b8d701e
egbejob680 <jobegbe39@gmail.com>
2022-10-24 21:54:03 +0100

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,19 @@
 #include "main.h"
+
 /**
-* _puts - print a string
-* @str: pointer to the string
-* Return: void
-*/
+ * _puts - prints a string
+ * @str: pointer to the string
+ * Return: void
+ */
 void _puts(char *str)
 {
-    int index = 0;
-    while (str[index] != '\0')
-    {
-        _putchar(str[index]);
-        index++;
-    }
-    _putchar('\0');
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+	}
+
+	_putchar('\n');
 }
