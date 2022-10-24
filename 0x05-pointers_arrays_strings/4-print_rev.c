5123c05154be572d1ca517ce607ce27dfe34790d
egbejob680 <jobegbe39@gmail.com>
2022-10-24 21:52:24 +0100

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
-* print_rev -print a string in a reverse order
-* @s: string
-* Return: void
-*/
+ * print_rev - print a string in reverse order
+ * @s: string
+ * Return: void
+ */
 void print_rev(char *s)
 {
-    char *tp = s;
-    char *i;
-    if (s[0] == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    while (*tp != '\0')
-    {
-        tp = tp + 1;
-    }
-    for (i = tp-1; i>=s; i -= 1)
-    {
-        _putchar(*i);
-    }
-    _putchar('\n');
+	char *tp = s;
+	char *i;
+
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (*tp != '\0')
+	{
+		tp = tp + 1;
+	}
+
+	for (i = tp - 1; i >= s; i -= 1)
+	{
+		_putchar(*i);
+	}
+	_putchar('\n');
 }
