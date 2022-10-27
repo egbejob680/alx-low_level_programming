87a896ac782ad08c5942f918550a176f97834917
egbejob680 <jobegbe39@gmail.com>
2022-10-27 20:39:26 +0100

    str

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,32 @@
+
 #include "main.h"
-#include <stdio.h>
+
 /**
-*_strcat - concatenate strings
-* @dest
-* @src
-* @n 
-* return a pointer to the resulting string
-*/
+ * _strncat - concatenates two string using a limited number of charcaters
+ * @dest: destintion string
+ * @src: source string
+ * @n: highest number of characters to the characters to be concatenated
+ * Return: pointer to the concatenated string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-int i,j;
-i = 0;
-j =0;
-while (dest[i] != '\0')
-i++;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-return dest;
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+
+		if (src[j] == '\0')
+			break;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
