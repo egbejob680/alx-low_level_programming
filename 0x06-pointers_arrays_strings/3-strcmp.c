ed81b52186e4ee5353c09ad67bd3f17083de3eff
egbejob680 <jobegbe39@gmail.com>
2022-10-27 20:51:27 +0100

    str

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * _strcmp
 *@s1
@@ -8,11 +9,13 @@
 int _strcmp(char *s1, char *s2)
 {
 while (*s1 == *s2)
+
 {
 if (*s1 == '\0')
 {
 return 0;
 }
+
 s1++;
 s2++;
 }
