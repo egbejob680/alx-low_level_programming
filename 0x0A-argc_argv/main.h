22209eb49ae963b1649c5933e439d0e03062c165
egbejob680 <jobegbe39@gmail.com>
2022-11-05 02:55:47 +0100

    main

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int);
+int minimum_coins(int);
+int _atoi(char *);
+void print_number(int);
+int check_number(char *);
+void print_error(char *);
+
+#endif
